ff5d6fb205e8f279313a38850d75cc4b15f409df
Anthony Zhou <Az040205@gmail.com>
2024-03-14 04:14:03 -0400

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,38 @@
+'use client'
+import styles from './page.module.scss'
+import { useState } from 'react';  
+import { motion } from 'framer-motion';
+import useMousePosition from './utils/useMousePosition';
+import { useEffect } from 'react';
+
+
+
+
+export default function Home() {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const { x, y } = useMousePosition();
+  const size = isHovered ? 400 : 40;
+
+  return (
+    <main className={styles.main}>
+      <motion.div 
+        className={styles.mask}
+        animate={{
+          WebkitMaskPosition: `${x - (size/2)}px ${y - (size/2)}px`,
+          WebkitMaskSize: `${size}px`,
+        }}
+        transition={{ type: "tween", ease: "backOut", duration:0.5}}
+      >
+          <p onMouseEnter={() => {setIsHovered(true)}} onMouseLeave={() => {setIsHovered(false)}}>
+          你好我是周嘉成
+          </p>
+      </motion.div>
+
+      <div className={styles.body}>
+        <p>I am <span>Anthony Zhou</span> .</p>
+      </div>
+
+    </main>
+  )
+}
